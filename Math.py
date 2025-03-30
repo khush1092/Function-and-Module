188e1411c8ea4a14a7d6d40a856f56e8f2e912e9
Khush Shah <khush82004@gmail.com>
2025-03-30 20:07:53 +0530

    Add files via upload

---
+++ Math.py
@@ -0,0 +1,8 @@
+# using math module
+from math import *
+num = int(input("Enter Number"))
+
+print("Number :",num," = ",sqrt(num))
+print("Number :",num," = ",log(num))
+print("Number :",num," = ",asin(num))
+    
